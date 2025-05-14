d528c378ae659e39cd6e7628808341eec6b3e1df
Jeff Barfield <jeffreybarfield@protonmail.com>
2025-05-14 01:11:09 -0400

    Decompose into library with example

---
+++ examples/conway/main.rs
@@ -0,0 +1,98 @@
+use bevy::{input::mouse::MouseWheel, prelude::*, window::WindowMode};
+use rand::Rng;
+
+use bevy_cgol::{Cell, ConwayPlugin, Coordinates, State};
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Conway's Game of Life".to_string(),
+                    mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+                    ..default()
+                }),
+                ..default()
+            }),
+            ConwayPlugin,
+        ))
+        .add_systems(Startup, (setup_camera, setup_world))
+        .add_systems(Update, (pan_camera, ev_mouse_wheel))
+        .run();
+}
+
+fn setup_world(mut commands: Commands) {
+    let mut rng = rand::rng();
+    for y in -200..200 {
+        for x in -200..200 {
+            let state = if rng.random_bool(0.1) {
+                State::Alive
+            } else {
+                State::Dead
+            };
+            commands.spawn((
+                Sprite {
+                    color: Color::srgba(255., 0., 0., 0.),
+                    ..default()
+                },
+                Cell,
+                state,
+                Coordinates(IVec2::new(x, y)),
+                Transform::from_translation(Vec3::new(x as f32, y as f32, 0.)),
+            ));
+        }
+    }
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((
+        Camera2d,
+        OrthographicProjection {
+            near: -1000.0,
+            scale: 0.11,
+            ..OrthographicProjection::default_2d()
+        },
+        MainCamera,
+    ));
+}
+
+pub fn pan_camera(
+    mut camera_query: Query<&mut Transform, With<MainCamera>>,
+    keys: Res<ButtonInput<KeyCode>>,
+) {
+    let mut transform = camera_query.single_mut();
+
+    if keys.pressed(KeyCode::KeyW) {
+        transform.translation.y += 2.;
+    }
+
+    if keys.pressed(KeyCode::KeyA) {
+        transform.translation.x -= 2.;
+    }
+
+    if keys.pressed(KeyCode::KeyS) {
+        transform.translation.y -= 2.;
+    }
+
+    if keys.pressed(KeyCode::KeyD) {
+        transform.translation.x += 2.;
+    }
+}
+
+pub fn ev_mouse_wheel(
+    mut ev_scroll: EventReader<MouseWheel>,
+    mut camera_query: Query<&mut OrthographicProjection, With<MainCamera>>,
+) {
+    if !ev_scroll.is_empty() {
+        let mut projection = camera_query.single_mut();
+        ev_scroll.read().for_each(|ev| {
+            let zoom = -(ev.y / 100.);
+            if projection.scale + zoom > 0.01 {
+                projection.scale += zoom;
+            }
+        });
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Default, Component, Reflect)]
+pub struct MainCamera;
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,20 @@
+use bevy::prelude::*;
 use bevy::utils::Duration;
 use bevy::utils::HashMap;
-use bevy::{input::mouse::MouseWheel, prelude::*, window::WindowMode};
-use rand::Rng;
 
-fn main() {
-    App::new()
-        .add_plugins((DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Conway's Game of Life".to_string(),
-                mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
-                ..default()
-            }),
-            ..default()
-        }),))
-        .init_resource::<ElapsedSteps>()
-        .insert_resource(ClearColor(Color::Srgba(Srgba::rgba_u8(49, 87, 113, 255))))
-        .init_resource::<StepTimer>()
-        .add_systems(Startup, (setup_camera, setup_world))
-        .add_systems(Update, (pan_camera, ev_mouse_wheel))
-        .add_systems(Update, step)
-        // .add_systems(Update, cleanup_orphaned_cells.after(step))
-        .add_systems(Update, color_cells.after(step))
-        .run();
+pub struct ConwayPlugin;
+
+impl Plugin for ConwayPlugin {
+    fn build(&self, app: &mut App) {
+        app.init_resource::<ElapsedSteps>()
+            .insert_resource(ClearColor(Color::Srgba(Srgba::rgba_u8(49, 87, 113, 255))))
+            .init_resource::<StepTimer>()
+            .add_systems(Update, step)
+            .add_systems(Update, color_cells.after(step));
+    }
 }
 
-fn step(
+pub fn step(
     mut elapsed_steps: ResMut<ElapsedSteps>,
     mut cell_query: Query<(Entity, &Cell, &mut State, &Coordinates)>,
     mut step_timer: ResMut<StepTimer>,
@@ -99,79 +89,6 @@ fn color_cells(mut cells_query: Query<(&Cell, &mut Sprite, &State)>) {
         });
 }
 
-fn setup_world(mut commands: Commands) {
-    let mut rng = rand::rng();
-    for y in -200..200 {
-        for x in -200..200 {
-            let state = if rng.random_bool(0.1) {
-                State::Alive
-            } else {
-                State::Dead
-            };
-            commands.spawn((
-                Sprite {
-                    color: Color::srgba(255., 0., 0., 0.),
-                    ..default()
-                },
-                Cell,
-                state,
-                Coordinates(IVec2::new(x, y)),
-                Transform::from_translation(Vec3::new(x as f32, y as f32, 0.)),
-            ));
-        }
-    }
-}
-
-fn setup_camera(mut commands: Commands) {
-    commands.spawn((
-        Camera2d,
-        OrthographicProjection {
-            near: -1000.0,
-            scale: 0.11,
-            ..OrthographicProjection::default_2d()
-        },
-        MainCamera,
-    ));
-}
-
-pub fn pan_camera(
-    mut camera_query: Query<&mut Transform, With<MainCamera>>,
-    keys: Res<ButtonInput<KeyCode>>,
-) {
-    let mut transform = camera_query.single_mut();
-
-    if keys.pressed(KeyCode::KeyW) {
-        transform.translation.y += 2.;
-    }
-
-    if keys.pressed(KeyCode::KeyA) {
-        transform.translation.x -= 2.;
-    }
-
-    if keys.pressed(KeyCode::KeyS) {
-        transform.translation.y -= 2.;
-    }
-
-    if keys.pressed(KeyCode::KeyD) {
-        transform.translation.x += 2.;
-    }
-}
-
-pub fn ev_mouse_wheel(
-    mut ev_scroll: EventReader<MouseWheel>,
-    mut camera_query: Query<&mut OrthographicProjection, With<MainCamera>>,
-) {
-    if !ev_scroll.is_empty() {
-        let mut projection = camera_query.single_mut();
-        ev_scroll.read().for_each(|ev| {
-            let zoom = -(ev.y / 100.);
-            if projection.scale + zoom > 0.01 {
-                projection.scale += zoom;
-            }
-        });
-    }
-}
-
 #[derive(Clone, Eq, PartialEq, Debug, Default, Resource, Reflect)]
 pub struct CellMap {
     map: HashMap<Coordinates, Alive>,
@@ -221,9 +138,6 @@ impl MooreNeighborhood {
         .map(move |delta| Coordinates(coordinates.0 + delta))
     }
 }
-
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Default, Component, Reflect)]
-pub struct MainCamera;
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug, Default, Component, Reflect)]
 pub struct Cell;
